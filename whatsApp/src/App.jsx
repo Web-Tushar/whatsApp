75af74d56737b2942f8de6b8ebada8fc32f7050b
tamim-devs <tamimh0727@gmail.com>
2024-07-31 22:34:52 +0600

    vai

--- whatsApp/src/App.jsx
+++ whatsApp/src/App.jsx
@@ -2,21 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <>
-      <div>ami tamim</div>
-      <div>ami tushar imran</div>
-      <h1>ami abar change korlam</h1>
-      <h2>ki obostha</h2>
-      <h3>ami sokalay install dibo</h3>
-<<<<<<< HEAD
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sed blanditiis exercitationem, dicta saepe quisquam? Officiis cumque enim dolorem. Quae.wwwwwwwwwwwww</h3>
-=======
-      <h3>ami sokalays install dekhbo </h3>
-      <h3>ami sokalays install dekhbo vai</h3>
-      <h3>ami  install dekhbo vai</h3>
-      <h3>ami  insthhhhhhhhhhh</h3>
->>>>>>> cfa529bc553ffaf1478df93c893e103844bdd446
-    </>
+    <div>
+
+      <h1>tshar vai sob kaitta dilam</h1>
+    </div>
   )
 }
 
